430d0031e0006a82963fc1598574beb746b252bf
timdegroot05 <336795@student.mboutrecht.nl>
2023-02-19 22:05:31 +0100

    style beetje aangepast
    6e commit

--- opdracht/index.php
+++ opdracht/index.php
@@ -8,7 +8,6 @@
     <title>Bling Bling Nail Studio Chantal</title>
 </head>
 <body>
-    
         <div>
             <h1>Bling Bling Nail Studio Chantal</h1>
                 <h2>Kies 4 basiskleuren voor uw nagels:</h2>
@@ -17,11 +16,11 @@
                     <input type="color">
                     <input type="color">
                 <h2>Uw telefoonnummer:</h2>
-                    <input type="tel" placeholder="+31 6 2341 87 41">
+                    <input type="tel" id="telefoonnummer" placeholder="+31 6 2341 87 41">
                 <h2>Uw e-mailadres:</h2>
-                    <input type="email" placeholder="kaas123@kaas123.com">
+                    <input type="email" id="e-mail" placeholder="kaas123@kaas123.com">
                 <h2>Afspraak datum/tijd:</h2>
-                    <input type="datetime-local">
+                    <input type="datetime-local" id="datum-tijd">
                 <h2>Soort behandeling:</h2>
                     <input type="checkbox">Nagelbijt arrangement (termijnbetaling mogelijk) €180 <br>
                     <input type="checkbox">Luxe manicure (massage en handpakking) €30,00 <br>
