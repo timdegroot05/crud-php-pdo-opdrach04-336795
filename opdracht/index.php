cf4da41965801867bfd5ef4bcef1ab3e78e6df99
timdegroot05 <336795@student.mboutrecht.nl>
2023-02-19 21:50:00 +0100

    3e commit
    3e commit

--- opdracht/index.php
+++ opdracht/index.php
@@ -8,13 +8,25 @@
     <title>Bling Bling Nail Studio Chantal</title>
 </head>
 <body>
-    <h1>Bling Bling Nail Studio Chantal</h1>
+    
         <div>
-            <h2>Kies 4 basiskleuren voor uw nagels</h2><br>
-            <input type="color">
-            <input type="color">
-            <input type="color">
-            <input type="color">
+            <h1>Bling Bling Nail Studio Chantal</h1>
+                <h2>Kies 4 basiskleuren voor uw nagels:</h2>
+                    <input type="color">
+                    <input type="color">
+                    <input type="color">
+                    <input type="color">
+                <h2>Uw telefoonnummer:</h2>
+                    <input type="tel" placeholder="+31 6 2341 87 41">
+                <h2>Uw e-mailadres:</h2>
+                    <input type="email" placeholder="kaas123@kaas123.com">
+                <h2>Afspraak datum/tijd:</h2>
+                    <input type="datetime-local">
+                <h2>Soort behandeling:</h2>
+                    <input type="checkbox">Nagelbijt arrangement (termijnbetaling mogelijk) €180 <br>
+                    <input type="checkbox">Luxe manicure (massage en handpakking) €30,00 <br>
+                    <input type="checkbox">nagelreparatie per nagel (in eerste week gratis) €5,00 <br><br>
+                    <input type="button" id="opslaan-button"> 
         </div>
 </body>
 </html>
