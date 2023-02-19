a42439c45825cfed81f040a45bcb0b64bee389ca
timdegroot05 <336795@student.mboutrecht.nl>
2023-02-19 21:28:31 +0100

    commits
    2e commit

---
+++ opdracht/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Bling Bling Nail Studio Chantal</title>
+</head>
+<body>
+    <h1>Bling Bling Nail Studio Chantal</h1>
+        <div>
+            <h2>Kies 4 basiskleuren voor uw nagels</h2><br>
+            <input type="color">
+            <input type="color">
+            <input type="color">
+            <input type="color">
+        </div>
+</body>
+</html>
